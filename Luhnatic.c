96ec773744a385fc29eb64fa833c7d75439cf8cd
Rithvik Praveen Kumar <rithvikpkx@gmail.com>
2026-08-12 21:38:26 -0400

    (code): Added basic CLI usage message logic.

--- Luhnatic.c
+++ Luhnatic.c
@@ -1,3 +1,14 @@
-int main () {
+#include <stdio.h>
+
+int main (int argv, char *argc[]) {
+    if (argv < 2) {
+        printf("USAGE:\n\
+                ./Luhnatic validate <16 digit num>\n\
+                ./Luhnatic generate <length>\n\
+                ./Luhnatic explain <16 digit num>\n");
+    }
+
+    printf("%s\n", argc[0]);
+
     return 0;
 }
