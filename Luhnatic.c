efae63465ae3193219e80d19383a206b2befa528
Rithvik Praveen Kumar <rithvikpkx@gmail.com>
2026-08-12 22:40:28 -0400

    (code): Implemented validate branch.

--- Luhnatic.c
+++ Luhnatic.c
@@ -4,24 +4,70 @@
 
 #define BAD_CLI_INVOCATION (1)
 
-long validate_num(long num) {
-    return num;
+int validate_num(long num) {
+    int sum = 0;
+
+    int sum_digit = 0;
+
+    while (num) {
+        if (sum_digit == 0) {
+            sum += num % 10;
+            num /= 10;
+            sum_digit = 1;
+        }
+        else {
+            int doubled = (num % 10) * 2;
+
+            if (doubled > 9) {
+                sum += doubled % 10;
+                doubled /= 10;
+                sum += doubled;
+            }
+            else {
+                sum += doubled;
+            }
+
+            sum_digit = 0;
+            num /= 10;
+        }
+    }
+
+    if (sum % 10 == 0) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 int main (int argv, char *argc[]) {
     if (argv <= 2 || argv > 3) {
         printf("USAGE:\n\
-                ./Luhnatic validate <16 digit num>\n\
+                ./Luhnatic validate <num>\n\
                 ./Luhnatic generate <length>\n\
-                ./Luhnatic explain <16 digit num>\n");
+                ./Luhnatic explain <num>\n");
 
         return BAD_CLI_INVOCATION;
     }
 
     if (strcmp(argc[1], "validate") == 0) {
-        printf("validate branch\n");
+        char *endptr = NULL;
+        long num = strtol(argc[2], &endptr, 10);
 
-        printf("Parsed Num: %ld\n", strtol(argc[2], NULL, 10));
+        if (endptr == argc[2] || *endptr != '\0') {
+            printf("INVALID\n");
+            printf("USAGE: ./Luhnatic validate <num>\n");
+        }
+        else {
+            int valid_num = validate_num(num);
+
+            if (valid_num) {
+                printf("---VALID LUHN NUM: %ld---\n", num);
+            }
+            else {
+                printf("---INVALID LUHN NUM: %ld---\n", num);
+            }
+        }
     }
     else if (strcmp(argc[1], "generate") == 0) {
         printf("generate branch\n");
